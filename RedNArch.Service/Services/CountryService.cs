1f112c999ec0c1c738a1d0e8799f87c29e4daa75
panickeranoop09 <panickeranoop09@gmail.com>
2016-11-02 10:31:13 +0530

    Add files via upload

---
+++ RedNArch.Service/Services/CountryService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedNArch.Model;
+using RedNArch.Repository;
+using RedNArch.Model.Model;
+using RedNArch.Repository.Interface;
+using RedNArch.Service.Interface;
+
+namespace RedNArch.Service.Services
+{
+    public class CountryService : EntityService<Country>, ICountryService
+    {
+        IUnitOfWork _UnitOfWork;
+        ICountryRepository _countryRepository;
+
+        public CountryService(IUnitOfWork UnitOfWork, ICountryRepository countryRepository):base(UnitOfWork, countryRepository)
+        {
+            _UnitOfWork = UnitOfWork;
+            _countryRepository = countryRepository;
+        }
+        public Country GetById(int Id)
+        {
+            return _countryRepository.GetById(Id);
+        }
+    }
+}
---
+++ RedNArch.Service/Services/PersonService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedNArch.Model.Model;
+using RedNArch.Repository.Interface;
+using RedNArch.Service.Interface;
+
+namespace RedNArch.Service.Services
+{
+    public class PersonService : EntityService<Person>, IPersonService
+    {
+        IUnitOfWork _UnitOfWork;
+        IPersonRepository _personRepository;
+        
+        public PersonService(IUnitOfWork UnitOfWork, IPersonRepository personRepository):base(UnitOfWork, personRepository)
+        {
+            _UnitOfWork = UnitOfWork;
+            _personRepository = personRepository;
+        }
+        public Person GetById(long Id)
+        {
+            return _personRepository.GetById(Id);
+        }
+    }
+}
